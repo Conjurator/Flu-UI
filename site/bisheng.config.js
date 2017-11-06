ba37c6a1707774bb1ed7b50113cdf139bb5c7d63
hefei00 <hefei00@outlook.com>
2017-11-06 14:40:09 +0800

    网站基本布局

--- site/bisheng.config.js
+++ site/bisheng.config.js
@@ -9,24 +9,8 @@ module.exports = {
     changelog: 'CHANGELOG.md'
   },
   plugins: ['bisheng-plugin-react?lang=__react'],
+  //webpack config,可在此覆盖默认配置
   webpackConfig(config) {
-    config.module.rules = config.module.rules.concat([
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]'
-        }
-      },
-      {
-        exclude: [/\.js$/, /\.css$/, /\.scss$/, /\.html$/, /\.json$/],
-        loader: require.resolve('file-loader'),
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]'
-        }
-      }
-    ])
     return config
   }
 }
--- site/theme/template/Footer.js
+++ site/theme/template/Footer.js
@@ -4,7 +4,7 @@ import '../static/Footer.scss'
 export default function Footer(props) {
   return <footer className="footer">
     <div className="footer-inner">
-      with ❤ by <a href="https://github.com/Conjurator" target="_blank">想看短视频FE</a>
+      with ❤ by <a href="https://github.com/Conjurator" rel="noopener noreferrer"  target="_blank">想看短视频FE</a>
     </div>
   </footer>
 }
