92912e801b3d366f5010fa4646a90da5cbcffeda
hefei00 <hefei00@outlook.com>
2017-11-20 15:03:54 +0800

    cover样式

--- site/theme/template/Cover.js
+++ site/theme/template/Cover.js
@@ -3,13 +3,27 @@ import {Link} from 'react-router'
 import '../static/Cover.scss'
 
 class Cover extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      width: '100%',
+      height: '100%'
+    }
+  }
+  componentDidMount () {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
   render () {
+    const {width, height} = this.state
     return (
-      <div className="cover-container">
+      <div className="cover-container" style={{width, height}}>
         <img src="" alt="" className="cover-logo"/>
         <h2 className="cover-title">FLU-UI</h2>
         <div className="cover-link">
-          <a className="link-github" target="_blank" href="https://github.com/Conjurator/Flu-UI">GitHub</a>
+          <a className="link-github" target="_blank" rel="noopener noreferrer" href="https://github.com/Conjurator/Flu-UI">GitHub</a>
           <Link className="link-start" to="/layout/doc/start">Start</Link>
         </div>
         <div className="cover-desc">
