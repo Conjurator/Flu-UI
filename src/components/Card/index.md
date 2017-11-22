e4d0ec6540e30050abd85ef37892978978027daf
hefei00 <hefei00@outlook.com>
2017-11-22 20:47:58 +0800

    carc story

---
+++ src/components/Card/index.md
@@ -0,0 +1,32 @@
+# Card
+卡片
+
+## 使用
+```js
+import { Card } from 'fluui'
+```
+
+<!-- STORY -->
+
+## 示例
+
+```jsx
+import { Card } from 'fluui'
+ReactDOM.render(
+  <div clasName={box}>
+    <Card title="hello">
+      <div>hahha</div>
+      <div>xixixi</div>
+    </Card>
+  </div>
+, mountNode)
+```
+
+## API
+
+| 属性 | 说明 | 类型 | 可选值 | 默认值 |
+| --- | --- | ---| --- | --- |
+| wrapClass | class类名 | string | - | - |
+| width | card宽度 | number | - | `360` |
+| height | card高度 | number | - | `120` |
+| title | card标题 | string | - | - |
--- stories/Card.js
+++ stories/Card.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Card from '../src/components/Card'
+import { withDocs } from 'storybook-readme'
+import ButtonDoc from '../src/components/Card/index.md'
 
 storiesOf('Card', module)
-  .add('default', () =>
-      <Card>
-        <div>这是一个card</div>
-      </Card>
-  )
-  .add('with title', () =>
+  .addDecorator(story => <div style={{ backgroundColor: '#eeeeee', position: 'relative', padding: 20 }}>{story()}</div>)
+  .addDecorator(withDocs(ButtonDoc))
+  .add('default', () => (
+    <Card>
+      <div>这是一个card</div>
+    </Card>
+  ))
+  .add('with title', () => (
     <Card title="新增访问量">
       <div>这是一个带titlle的card</div>
     </Card>
-  )
+  ))
